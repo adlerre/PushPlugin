0f9319845fe6f5ce3d88995054276cb060aaf06b
adlerre <rene.adler@electric-force.net>
2013-08-20 18:15:04 +0200

    rework js plugin

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,61 +1,76 @@
+/*
+ * 
+ * Copyright 2013 René Adler
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-var argscheck = require('cordova/argscheck'),
-    exec = require('cordova/exec');
+var argscheck = require('cordova/argscheck'), exec = require('cordova/exec');
 
 var PushNotification = function() {
-};
 
+    /**
+     * Call this to register for push notifications. Content of [options]
+     * depends on whether we are working with APNS (iOS) or GCM (Android)
+     * 
+     * @param {Function}
+     *                successCallback
+     * @param {Function}
+     *                errorCallback
+     * @param {Object}
+     *                options
+     */
+    this.register = function(successCallback, errorCallback, options) {
+	argscheck.checkArgs('fF', 'PushPlugin.register', arguments);
+	cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [ options ]);
+    };
 
-// Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-PushNotification.prototype.register = function(successCallback, errorCallback, options) {
-    if (errorCallback == null) { errorCallback = function() {}}
+    /**
+     * Call this to unregister for push notifications
+     * 
+     * @param {Function}
+     *                successCallback
+     * @param {Function}
+     *                errorCallback
+     */
+    this.unregister = function(successCallback, errorCallback) {
+	argscheck.checkArgs('fF', 'PushPlugin.unregister', arguments);
+	cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
+    };
 
-    if (typeof errorCallback != "function")  {
-        console.log("PushNotification.register failure: failure parameter not a function");
-        return
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("PushNotification.register failure: success callback parameter must be a function");
-        return
-    }
-
-	cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
-};
-
-// Call this to unregister for push notifications
-PushNotification.prototype.unregister = function(successCallback, errorCallback) {
-    if (errorCallback == null) { errorCallback = function() {}}
-
-    if (typeof errorCallback != "function")  {
-        console.log("PushNotification.unregister failure: failure parameter not a function");
-        return
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("PushNotification.unregister failure: success callback parameter must be a function");
-        return
-    }
-
-     cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", []);
-};
- 
- 
-// Call this to set the application icon badge
-PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, errorCallback, badge) {
-    if (errorCallback == null) { errorCallback = function() {}}
-
-    if (typeof errorCallback != "function")  {
-        console.log("PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function");
-        return
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function");
-        return
-    }
-
-    cordova.exec(successCallback, successCallback, "PushPlugin", "setApplicationIconBadgeNumber", [{badge: badge}]);
+    /**
+     * Call this to set the application icon badge
+     * 
+     * @param {Function}
+     *                successCallback
+     * @param {Function}
+     *                errorCallback
+     * @param {Object}
+     *                badge
+     */
+    this.setApplicationIconBadgeNumber = function(successCallback, errorCallback, badge) {
+	argscheck.checkArgs('fF', 'PushPlugin.setApplicationIconBadgeNumber', arguments);
+	cordova.exec(successCallback, errorCallback, "PushPlugin", "setApplicationIconBadgeNumber", [ {
+	    badge : badge
+	} ]);
+    };
 };
 
 module.exports = new PushNotification();
